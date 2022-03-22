198d1f0b9319ca1c813bb18a7e13357d09851ca8
MohammadAlhawamdeh <mohammed.alhawamdeh@and.digitalm>
2022-03-22 14:33:52 +0000

    Adding a theme

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Accordion } from "../components/Accordion";
-
+import 'bootswatch/dist/slate/bootstrap.min.css';
 function App() {
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./containers/App";
-
+import "bootswatch/dist/flatly/bootstrap.min.css";
 ReactDOM.render(
   <React.StrictMode>
     <App />
